e4da5bec9c60797bf61527277982851128422854
Patryk Ignaczak <75589318+igpatryk@users.noreply.github.com>
2021-09-23 03:44:12 +0200

    Update main.py
    gn

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from requests_html import HTMLSession
 session = HTMLSession()
 resp = session.get("https://justjoin.it/warszawa/python/junior?tab=with-salary")
 resp.html.render()
-dividedbypln = resp.html.html.split(' PLN')
-for element in dividedbypln:
+divided_by_pln = resp.html.html.split(' PLN')
+list_of_salaries = []
+for element in divided_by_pln:
     if element[-1]=='k':
-        print(element[-16:].split(">",1)[1])
+        salary = element[-16:].split(">", 1)[1]
+        list_of_salaries.append(salary)
+print(list_of_salaries)
+#for element in list_of_salaries:
+#    x = element.split("k")[0]
+#    print(str(x))
+#    print(str(x).isnumeric())
