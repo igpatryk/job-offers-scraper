be4f57d554f6cc56ca0eb99445347ed7b9861efa
Patryk Ignaczak <75589318+igpatryk@users.noreply.github.com>
2021-09-23 03:28:48 +0200

    Update main.py
    program prints salaries of 7 latest job offers. progress!

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ from requests_html import HTMLSession
 session = HTMLSession()
 resp = session.get("https://justjoin.it/warszawa/python/junior")
 resp.html.render()
-print(resp.html.html)
+dividedbypln = resp.html.html.split(' PLN')
+for element in dividedbypln:
+    if element[-1]=='k':
+        print(element[-16:].split(">",1)[1])
