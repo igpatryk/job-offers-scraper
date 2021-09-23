59659d9ca3103f0c6524f94ea4ed9971308a3d1c
Patryk Ignaczak <75589318+igpatryk@users.noreply.github.com>
2021-09-23 03:33:22 +0200

    Update main.py
    changed the link so now program prints latest 9 salaries from job offers.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from requests_html import HTMLSession
 
 session = HTMLSession()
-resp = session.get("https://justjoin.it/warszawa/python/junior")
+resp = session.get("https://justjoin.it/warszawa/python/junior?tab=with-salary")
 resp.html.render()
 dividedbypln = resp.html.html.split(' PLN')
 for element in dividedbypln:
