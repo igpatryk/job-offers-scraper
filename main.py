ccaee692c90de84e6f1175b064a2e185fa6b9e98
Patryk Ignaczak <75589318+igpatryk@users.noreply.github.com>
2021-09-27 12:48:26 +0200

    Update main.py
    this time it is mainly a refactor of a code + now I limit the offers by discarding offers after string "Python in other cities"

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 from time import sleep
 from selenium import webdriver
 
-browser = webdriver.Chrome(executable_path=r'PATH_TO_CHROMEDRIVER')
-browser.get('https://justjoin.it/warszawa/python/junior?tab=with-salary')
-sleep(3)
-browser.execute_script("document.body.style.zoom='50%'")
-resp = browser.page_source
-divided_by_pln = resp.split(' PLN')
-list_of_salaries = []
-sum_of_salaries = 0
-counter_of_salaries = 0
-for element in divided_by_pln:
-    if element[-1]=='k':
-        salary = element[-16:].split(">", 1)[1]
-        salary2 = salary.replace("k", "")
-        salary = salary2.replace(" ", "")
-        salary2 = salary.replace("-", " ")
-        salary = salary2.split(" ")
-        sum_of_actual_salaries = 0
-        for i in range(0, len(salary)):
-            sum_of_actual_salaries = sum_of_actual_salaries + float(salary[i])
-        mean_of_actual_salaries = sum_of_actual_salaries/len(salary)
-        sum_of_salaries = sum_of_salaries + mean_of_actual_salaries
-        counter_of_salaries = counter_of_salaries + 1
-print(sum_of_salaries/counter_of_salaries)
+
+def scrap_justjoinit(link):
+    browser = webdriver.Chrome()
+    browser.get(link)
+    sleep(3)
+    browser.execute_script("document.body.style.zoom='50%'")
+    resp = browser.page_source
+    # I want to check offers only in specific region, so I need to discard offers in other cities
+    useful_resp = resp.split("Python</span> in other cities")[0]
+    return useful_resp
+
+
+def get_average_salary_justjoinit(junior_python_dev_jobs_in_warsaw):
+    offers_divided_by_pln = junior_python_dev_jobs_in_warsaw.split(" PLN")
+    sum_of_salaries = 0
+    counter_of_salaries = 0
+    for element in offers_divided_by_pln:
+        if element[-1] == "k":
+            salary = element[-16:].split(">", 1)[1]
+            salary2 = salary.replace("k", "")
+            salary = salary2.replace(" ", "")
+            salary2 = salary.replace("-", " ")
+            salary = salary2.split(" ")
+            sum_of_actual_salaries = 0
+            for i in range(0, len(salary)):
+                sum_of_actual_salaries = sum_of_actual_salaries + float(salary[i])
+                sum_of_actual_salaries = round(sum_of_actual_salaries, 2)
+            mean_of_actual_salaries = round(sum_of_actual_salaries / len(salary), 2)
+            sum_of_salaries = sum_of_salaries + mean_of_actual_salaries
+            counter_of_salaries = counter_of_salaries + 1
+    return round(sum_of_salaries / counter_of_salaries, 2)
+
+
+junior_python_dev_jobs_in_warsaw = scrap_justjoinit('https://justjoin.it/warszawa/python/junior?tab=with-salary')
+average_junior_python_dev_in_Warsaw_salary = get_average_salary_justjoinit(junior_python_dev_jobs_in_warsaw)
+print(average_junior_python_dev_in_Warsaw_salary)
