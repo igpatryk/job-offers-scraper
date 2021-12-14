aa2ab3ea11942b8855ebeb36ce996b5ab21c48d4
igpatryk <igpatryk7@gmail.com>
2021-12-14 19:58:34 +0100

    New feature: creating daily and monthly reports, sending an email. Changes: reaarange of the file, disable matplotlib font_manager logging, make the graph bigger Next step: configure the buddy.works pipeline, arrange the website where graph could be uploaded

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 from ftplib import FTP
 import os
+import smtplib
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 log_file = 'scrapper_' + str(datetime.utcnow().strftime('%Y_%m_%d')) + '.log'
 logging.basicConfig(filename=log_file, encoding='utf=8', level=logging.DEBUG, format='%(asctime)s | %(levelname)s'''
                                                                                      ' | %(message)s')
-conn = psycopg2.connect(
-    host=os.environ.get('pg_host'),
-    database=os.environ.get('pg_database'),
-    user=os.environ.get('pg_user'),
-    password=os.environ.get('pg_password')
-)
+logging.getLogger('matplotlib.font_manager').disabled = True
 
 
 def scrap_justjoinit(link):
@@ -60,29 +60,16 @@ def get_average_salary_justjoinit(offers):
         return 0
 
 
-def insert_data(date, warsaw, remote, avg):
-    try:
-        logging.info("Trying to insert data to PGSQL.")
-        sql = """INSERT INTO salaries.justjoinit(date, warsaw, remote, avg) VALUES (%s,%s,%s,%s);"""
-        data = (date, warsaw, remote, avg)
-        cur = conn.cursor()
-        cur.execute(sql, data)
-        conn.commit()
-        logging.info("Record inserted successfully into justjoinit table.")
-    except (Exception, psycopg2.Error) as error:
-        logging.error(error)
-    select_data()
-
-
 def process_justjoinit_data():
     average_junior_python_dev_salary_on_justjoinit = None
     junior_python_dev_jobs_offers_in_warsaw = scrap_justjoinit(
         'https://justjoin.it/warszawa/python/junior?tab=with-salary')
-    average_junior_python_dev_in_warsaw_salary = get_average_salary_justjoinit(junior_python_dev_jobs_offers_in_warsaw)
+    average_junior_python_dev_in_warsaw_salary = round(get_average_salary_justjoinit(
+        junior_python_dev_jobs_offers_in_warsaw), 2)
     junior_python_dev_jobs_offers_remote_poland = scrap_justjoinit(
         'https://justjoin.it/remote-poland/python/junior?tab=with-salary')
-    average_junior_python_dev_remote_poland_salary = get_average_salary_justjoinit(
-        junior_python_dev_jobs_offers_remote_poland)
+    average_junior_python_dev_remote_poland_salary = round(get_average_salary_justjoinit(
+        junior_python_dev_jobs_offers_remote_poland), 2)
     if average_junior_python_dev_remote_poland_salary != 0 and average_junior_python_dev_in_warsaw_salary != 0:
         if average_junior_python_dev_remote_poland_salary > average_junior_python_dev_in_warsaw_salary:
             logging.info("SCRAP RESULT: It is better to look for offers in category 'Remote Poland'! Average salary "
@@ -100,23 +87,67 @@ def process_justjoinit_data():
                      "cording to offers on ""justjoin.it is {}.zł"
                      .format(average_junior_python_dev_remote_poland_salary))
     if average_junior_python_dev_in_warsaw_salary != 0 and average_junior_python_dev_remote_poland_salary != 0:
-        average_junior_python_dev_salary_on_justjoinit = (average_junior_python_dev_in_warsaw_salary +
-                                                          average_junior_python_dev_remote_poland_salary) / 2
+        average_junior_python_dev_salary_on_justjoinit = round(((average_junior_python_dev_in_warsaw_salary +
+                                                          average_junior_python_dev_remote_poland_salary) / 2), 2)
     elif average_junior_python_dev_in_warsaw_salary != 0 and average_junior_python_dev_remote_poland_salary == 0:
         average_junior_python_dev_salary_on_justjoinit = average_junior_python_dev_in_warsaw_salary
     elif average_junior_python_dev_in_warsaw_salary == 0 and average_junior_python_dev_remote_poland_salary != 0:
         average_junior_python_dev_salary_on_justjoinit = average_junior_python_dev_remote_poland_salary
-    insert_data(datetime.today().strftime('%Y-%m-%d'), average_junior_python_dev_in_warsaw_salary,
-                average_junior_python_dev_remote_poland_salary, average_junior_python_dev_salary_on_justjoinit)
+    return average_junior_python_dev_in_warsaw_salary, average_junior_python_dev_remote_poland_salary, \
+        average_junior_python_dev_salary_on_justjoinit
 
 
-def upload_to_ftp():
-    ftp = FTP(os.environ.get('ftp_address'))
-    ftp.login(user=os.environ.get('ftp_user'), passwd=os.environ.get('ftp_password'))
-    ftp.encoding = "utf-8"
-    with open('graph.png', 'rb') as file:
-        ftp.storbinary('STOR graph.png', file)
-    ftp.quit()
+def insert_data(date, warsaw, remote, avg):
+    conn = psycopg2.connect(
+        host=os.environ.get('pg_host'),
+        database=os.environ.get('pg_database'),
+        user=os.environ.get('pg_user'),
+        password=os.environ.get('pg_password')
+    )
+    try:
+        logging.info("Trying to insert data to PGSQL.")
+        sql = """INSERT INTO salaries.justjoinit(date, warsaw, remote, avg) VALUES (%s,%s,%s,%s);"""
+        data = (date, warsaw, remote, avg)
+        cur = conn.cursor()
+        cur.execute(sql, data)
+        conn.commit()
+        logging.info("Record inserted successfully into justjoinit table.")
+    except (Exception, psycopg2.Error) as error:
+        logging.error(error)
+
+
+def select_data(sql):
+    conn = psycopg2.connect(
+        host=os.environ.get('pg_host'),
+        database=os.environ.get('pg_database'),
+        user=os.environ.get('pg_user'),
+        password=os.environ.get('pg_password')
+    )
+    cur = None
+    try:
+        query = sql
+        cur = conn.cursor()
+        cur.execute(query)
+        rows = cur.fetchall()
+        dates = []
+        warsaw = []
+        remote = []
+        avg = []
+        for row in rows:
+            dates.append(row[0])
+            warsaw.append(row[1])
+            remote.append(row[2])
+            avg.append(row[3])
+        cur.close()
+        conn.close()
+        return dates, warsaw, remote, avg
+    except (Exception, psycopg2.Error) as error:
+        logging.error(error)
+    finally:
+        if conn:
+            cur.close()
+            conn.close()
+            logging.info("Closed PGSQL Connection.")
 
 
 def make_graph(dates, warsaw, remote, avg):
@@ -136,39 +167,104 @@ def make_graph(dates, warsaw, remote, avg):
     ax.bar_label(warsaw, padding=3)
     ax.bar_label(avg, padding=3)
     ax.bar_label(remote, padding=3)
-    fig.tight_layout()
+    plt.gcf().set_size_inches(20, 10)
     plt.savefig('graph.png')
-    upload_to_ftp()
 
 
-def select_data():
-    cur = None
+def upload_to_ftp():
+    ftp = FTP(os.environ.get('ftp_address'))
+    ftp.login(user=os.environ.get('ftp_user'), passwd=os.environ.get('ftp_password'))
+    ftp.encoding = "utf-8"
+    with open('graph.png', 'rb') as file:
+        ftp.storbinary('STOR graph.png', file)
+    ftp.quit()
+
+
+def create_report(remote, warsaw, avg, monthly):
+    global smtp_mail
+    smtp_mail = os.environ.get('smtp_mail')
+    global receiver_mail
+    receiver_mail = os.environ.get('my_mail')
+    daily_subject = "Daily salaries-scrapper's report"
+    daily_body = "Hi! It's me, salaries-scrapper.\n" \
+           "Today:\n" \
+           "- the average salary of Junior Python Developer who works in Warsaw is {}zł.\n" \
+           "- the average salary of Junior Python Developer who works remotely is {}zł.\n" \
+           "- the average of these salaries is {}zł.\n" \
+           "This data was scrapped from justjoin.it website.\n" \
+           "There is a graph for the last 7 days in the attachment, take a look!".format(warsaw, remote, avg)
+    monthly_subject = "Monthly salaries-scrapper's report"
+    monthly_body = "Hi! It's me, salaries-scrapper.\n" \
+           "In the previous month:\n" \
+           "- the average salary of Junior Python Developer who works in Warsaw was {}zł.\n" \
+           "- the average salary of Junior Python Developer who works remotely was {}zł.\n" \
+           "- the average of these salaries was {}zł.\n" \
+           "This data was scrapped from justjoin.it website.\n" \
+           "There is a graph for the last one month in the attachment, take a look!".format(warsaw, remote, avg)
+    message = MIMEMultipart()
+    message["From"] = smtp_mail
+    message["To"] = receiver_mail
+    if monthly == 0:
+        message["Subject"] = daily_subject
+        message.attach(MIMEText(daily_body, "plain"))
+    else:
+        message["Subject"] = monthly_subject
+        message.attach(MIMEText(monthly_body, "plain"))
+    with open('graph.png', 'rb') as attachment:
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+    encoders.encode_base64(part)
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= graph.png",
+    )
+    message.attach(part)
+    text = message.as_string()
+    return text
+
+
+def send_mail(msg):
     try:
-        query = "SELECT DISTINCT * from salaries.justjoinit where date >= now() - interval '7' day order by date ASC"
-        cur = conn.cursor()
-        cur.execute(query)
-        rows = cur.fetchall()
-        dates = []
-        warsaw = []
-        remote = []
-        avg = []
-        for row in rows:
-            dates.append(row[0])
-            warsaw.append(row[1])
-            remote.append(row[2])
-            avg.append(row[3])
-        cur.close()
-        conn.close()
-        make_graph(dates, warsaw, remote, avg)
-    except (Exception, psycopg2.Error) as error:
-        logging.error(error)
-    finally:
-        if conn:
-            cur.close()
-            conn.close()
-            logging.info("Closed PGSQL Connection.")
-
+        smtp_server = 'smtp.gmail.com'
+        smtp_port = 587
+        smtp_password = os.environ.get('smtp_password')
+        logging.info("Trying to send an email.")
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(smtp_mail, smtp_password)
+            server.sendmail(smtp_mail, receiver_mail, msg)
+            server.quit()
+    except Exception as e:
+        logging.error(e)
+    else:
+        logging.info("Successfully send an email.")
 
 logging.info("Started job-offers-scrapper.")
-process_justjoinit_data()
+# get salaries
+average_junior_python_dev_in_warsaw_salary, average_junior_python_dev_remote_poland_salary, \
+average_junior_python_dev_salary_on_justjoinit = process_justjoinit_data()
+# insert salaries into pgsql database with today's date
+insert_data(datetime.today().strftime('%Y-%m-%d'), average_junior_python_dev_in_warsaw_salary,
+            average_junior_python_dev_remote_poland_salary, average_junior_python_dev_salary_on_justjoinit)
+# select data from pgsql database for latest 7 days
+select = "SELECT DISTINCT * from salaries.justjoinit where date >= now() - interval '7' day order by date ASC"
+dates, warsaw, remote, avg = select_data(select)
+# make a graph
+make_graph(dates, warsaw, remote, avg)
+# upload file to the ftp
+upload_to_ftp()
+# create daily report
+message = create_report(average_junior_python_dev_remote_poland_salary, average_junior_python_dev_in_warsaw_salary, average_junior_python_dev_salary_on_justjoinit, 0)
+# send daily report
+send_mail(message)
+# monthly report
+if datetime.today().strftime('%d') == '01':
+    select = "SELECT DISTINCT * from salaries.justjoinit where date >= now() - interval '1 months' order by date ASC"
+    dates, warsaw, remote, avg = select_data(select)
+    make_graph(dates, warsaw, remote, avg)
+    monthly_remote_avg = sum(remote) / len(remote)
+    monthly_warsaw_avg = sum(warsaw) / len(warsaw)
+    monthly_avg_avg = sum(avg) / len(avg)
+    message = create_report(monthly_remote_avg, monthly_warsaw_avg, monthly_avg_avg, 1)
+    send_mail(message)
 logging.info("Stopped job-offers-scrapper.")
